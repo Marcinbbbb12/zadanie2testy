9803a507324c3a7a85f8e0b5331a782aa8c907cf
Marcin <uT_3777@zsp3zamosc>
2025-05-12 12:00:56 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace zadanie2testy
+{
+    public class AbsoluteValueCalculator
+    {
+        public int GetAbsoluteValue(int number)
+        {
+            return Math.Abs(number);
+        }
+    }
+
+  
+    public class AbsoluteValueCalculatorTest
+    {
+        private AbsoluteValueCalculator _calculator;
+
+     
+        public void Setup()
+        {
+            _calculator = new AbsoluteValueCalculator();
+        }
+
+    
+        public void GetAbsoluteValue_PositiveNumber_ReturnsSameNumber()
+        {
+            Assert.AreEqual(5, _calculator.GetAbsoluteValue(5));
+        }
+
+      
+        public void GetAbsoluteValue_NegativeNumber_ReturnsPositiveNumber()
+        {
+            Assert.AreEqual(5, _calculator.GetAbsoluteValue(-5));
+        }
+
+       
+        public void GetAbsoluteValue_Zero_ReturnsZero()
+        {
+            Assert.AreEqual(0, _calculator.GetAbsoluteValue(0));
+        }
+    }
+}
